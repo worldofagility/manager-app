88aaef8f76c62d895150ec6a0e81e1aa3b0b7a2b
hung-hq <vn.hung.hq@gmail.com>
2021-11-08 20:29:18 +0700

    fix bug server

--- server/index.js
+++ server/index.js
@@ -12,11 +12,13 @@ const app = express();
 
 app.use(cors());
 
+//GET POST PUT DELETE PATCH
+
 mongoose
-  .connect(DB_URL)
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`listening on port ${PORT}`);
     });
   })
-  .error((err) => console.error(err));
+  .catch((err) => console.error(err));
